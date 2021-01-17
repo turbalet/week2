640b283ecb2d0153139f7d0edd5f1081bd565ed3
yungproger <dchinasylov@mail.ru>
2021-01-17 16:26:08 +0600

    end of 4

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,57 +1,67 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
+	"se07.com/pkg/models"
 	"strconv"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		app.notFound(w)
+		http.NotFound(w, r)
 		return
 	}
-
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"ui/html/footer.partial.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.errorLog.Println(err.Error())
-		app.serverError(w, err)
-		return
-	}
-
-	err = ts.Execute(w, nil)
+	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
-	}
-}
-
-func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
+	for _, snippet := range s {
+		fmt.Fprintf(w, "%v\n", snippet)
+	}
+	/*
+		files := []string{
+			"./ui/html/home.page.tmpl",
+			"./ui/html/base.layout.tmpl",
+			"./ui/html/footer.partial.tmpl",
+		}
 
-	w.Write([]byte("Creating snippet"))
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			app.serverError(w, err)
+			return
+		}
+
+		err = ts.Execute(w, nil)
+		if err != nil {
+			app.serverError(w, err)
+		}*/
 }
-
-func snippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Displaying snippet"))
-}
-
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
 	}
-	fmt.Fprintf(w, "Displaying a specific snippet with ID %d...", id)
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	fmt.Fprintf(w, "%v", s)
+}
+
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
 }
--- pkg/models/postgres/snippets.go
+++ pkg/models/postgres/snippets.go
@@ -1,8 +1,11 @@
 package postgres
 
 import (
+	"context"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"se07.com/pkg/models"
+	"strconv"
+	"time"
 )
 
 type SnippetModel struct {
@@ -10,17 +13,54 @@ type SnippetModel struct {
 }
 
 const (
-	snippetInsert = "INSERT INTO snippets (title, content, created, expires) VALUES($1, $2, $3,$4) RETURNING id"
+	insertSql = "INSERT INTO snippets (title, content, created, expires) VALUES($1, $2, $3,$4) RETURNING id"
+	select1   = "select id,title, content, created, expires from snippets where id=$1"
+	select2   = "SELECT id,title, content, created, expires from snippets where expires>now() order by created DESC LIMIT 10"
 )
 
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	var id uint64
+	days, err := strconv.Atoi(expires)
+	if err != nil {
+		return 0, err
+	}
+	row := m.DB.QueryRow(context.Background(), insertSql, title, content, time.Now(), time.Now().AddDate(0, 0, days))
+	err = row.Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+	return int(id), nil
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
-	return nil, nil
+	s := &models.Snippet{}
+	err := m.DB.QueryRow(context.Background(), select1, id).Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if err.Error() == "no rows in result set" {
+			return nil, models.ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+	return s, nil
 }
 
 func (m *SnippetModel) Latest() ([]*models.Snippet, error) {
-	return nil, nil
+	snippets := []*models.Snippet{}
+	rows, err := m.DB.Query(context.Background(), select2)
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		s := &models.Snippet{}
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+		snippets = append(snippets, s)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return snippets, nil
 }
